1f97481264c9344664a67acb608c53b3687215da
malmutairi <mohammed@sbit.sa>
2019-01-10 02:42:47 +0300

    upload scss files

--- src/app/organizer/my-events/my-events.component.ts
+++ src/app/organizer/my-events/my-events.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-events',
   templateUrl: './my-events.component.html',
-  styleUrls: ['./my-events.component.sass']
+  styleUrls: ['./my-events.component.scss']
 })
 export class MyEventsComponent implements OnInit {
 
