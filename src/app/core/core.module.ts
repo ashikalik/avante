38965129943d60adc73db403cb3963f81aa9c164
malmutairi <mohammed@sbit.sa>
2019-01-10 00:25:51 +0300

    Adding Network Layer And Core Module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LanguageSettingService} from "./language-setting.service";
+import {UserAuthService} from "./user-auth.service";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule
+    ],
+    providers: [UserAuthService, LanguageSettingService]
 })
-export class CoreModule { }
+export class CoreModule {
+}
