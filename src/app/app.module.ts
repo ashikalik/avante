caac966866cf4311b561b958e238fe6dcddfa28c
khaled Alkathiri <khaled@sbit.sa>
2019-01-10 23:10:47 +0300

    auth modules changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import * as $ from 'jquery';
-import {AboutUsModule} from './about-us/about-us.module';
-import {AuthModule} from './auth/auth.module';
-import {BuyTicketModule} from './buy-ticket/buy-ticket.module';
-import {CalendarModule} from './calendar/calendar.module';
-import {ContactUsModule} from './contact-us/contact-us.module';
-import {FaqModule} from './faq/faq.module';
-import {HomeModule} from './home/home.module';
-import {MyProfileModule} from './my-profile/my-profile.module';
-import {OrganizerModule} from './organizer/organizer.module';
-import {SearchEventsModule} from './search-events/search-events.module';
-import {ViewEventModule} from './view-event/view-event.module';
+// import {AboutUsModule} from './about-us/about-us.module';
+// import {AuthModule} from './auth/auth.module';
+// import {BuyTicketModule} from './buy-ticket/buy-ticket.module';
+// import {CalendarModule} from './calendar/calendar.module';
+// import {ContactUsModule} from './contact-us/contact-us.module';
+// import {FaqModule} from './faq/faq.module';
+// import {HomeModule} from './home/home.module';
+// import {MyProfileModule} from './my-profile/my-profile.module';
+// import {OrganizerModule} from './organizer/organizer.module';
+// import {SearchEventsModule} from './search-events/search-events.module';
+// import {ViewEventModule} from './view-event/view-event.module';
 import {CoreModule} from "./core/core.module";
 
 
@@ -24,16 +24,16 @@ import {CoreModule} from "./core/core.module";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        AboutUsModule,
-        AuthModule,
-        BuyTicketModule,
-        CalendarModule,
-        ContactUsModule,
-        FaqModule,
-        MyProfileModule,
-        OrganizerModule,
-        SearchEventsModule,
-        ViewEventModule,
+        // AboutUsModule,
+        // AuthModule,
+        // BuyTicketModule,
+        // CalendarModule,
+        // ContactUsModule,
+        // FaqModule,
+        // MyProfileModule,
+        // OrganizerModule,
+        // SearchEventsModule,
+        // ViewEventModule,
         CoreModule
 
     ],
