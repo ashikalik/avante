5e26f94b91c177097dbe74b0538c7513865481c9
khaled Alkathiri <khaled@sbit.sa>
2019-01-12 02:42:25 +0300

    register member & org & vlou

--- src/app/auth/register-user/register-user.component.ts
+++ src/app/auth/register-user/register-user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter,Input } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  @Output() onCancel: EventEmitter<any> = new EventEmitter();
+  @Output() onRegister: EventEmitter<any> = new EventEmitter();
 
+  public signupForm: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    public router: Router) {
+  }
   ngOnInit() {
+    this.initForm();
   }
 
+  public initForm() {
+    this.signupForm = this.formBuilder.group(
+      {
+        'first_name': ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(40)])],
+        'last_name': ['', Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(40)])],
+        'gender': ['', Validators.compose([Validators.required])],
+        'email': ['', Validators.compose([Validators.required, Validators.email, Validators.minLength(3)])],
+        'mobile': ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(10)])],
+        'password': ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+        'confirm_password': ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+        'recaptcha': ['', Validators.compose([Validators.required])],
+        'agreementChecked': [false, Validators.pattern('true')]
+      },
+      {
+        validator: this.passwordConfirm
+      });
+  }
+
+
+  public passwordConfirm(AC: AbstractControl): { invalid: boolean } {
+    if (AC.get('password').value !== AC.get('confirm_password').value) {
+      return { invalid: true };
+    }
+  }
+
+
+  onCancelButton() {
+    this.onCancel.emit();
+  }
+
+
+  onRegisterButton(form: any) {
+    console.log(form.value);
+    const body = {
+      first_name: form.value.first_name,
+      last_name: form.value.last_name,
+      email: form.value.email,
+      mobile: form.value.mobile,
+      password: form.value.password,
+      confirm_password: form.value.confirm_password,
+      user_type: 5,
+      gender: form.value.gender,
+      recaptcha: form.value.recaptcha
+    };
+    this.onRegister.emit(body);
+  }
 }
