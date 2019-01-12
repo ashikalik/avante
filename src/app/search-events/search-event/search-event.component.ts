0450e9ef24ea0ac3f8bd28cedc0063ed967b77e6
khaled Alkathiri <khaled@sbit.sa>
2019-01-13 01:16:18 +0300

    search

--- src/app/search-events/search-event/search-event.component.ts
+++ src/app/search-events/search-event/search-event.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from "../../api-services/common.service";
+import { City } from "../../models/city";
+import { Observable } from "rxjs";
+import { EventType } from "../../models/event-type";
+import { EventService } from "../../api-services/event.service";
+import { LatestEvent } from "../../models/latest-event";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-search-event',
@@ -7,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchEventComponent implements OnInit {
 
-  constructor() { }
+  public city$: Observable<City>;
+  public eventTypes$: Observable<EventType>;
+  public latestEvent$: Observable<LatestEvent>;
+
+  constructor(public commonService: CommonService,
+    public eventService: EventService,
+    public router: Router) {
+  }
 
   ngOnInit() {
+    this.getCity();
+    this.getEventType();
   }
+
+  getCity(): void {
+    this.city$ = this.commonService.getCity().pipe();
+  }
+
+  getEventType(): void {
+    this.eventTypes$ = this.commonService.getEventType().pipe();
+  }
+
 
 }
--- src/app/search-events/search-events.module.ts
+++ src/app/search-events/search-events.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {CommonService} from "../api-services/common.service";
+import {NetworkLayerModule} from "../network-layer/network-layer.module";
+import {EventService} from "../api-services/event.service";
+
 
 import { SearchEventsRoutingModule } from './search-events-routing.module';
 import { SearchEventComponent } from './search-event/search-event.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { EventListComponent } from './event-list/event-list.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [SearchEventComponent, SearchBarComponent, EventListComponent],
   imports: [
     CommonModule,
-    SearchEventsRoutingModule
-  ]
+    SearchEventsRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    NetworkLayerModule,
+    TranslateModule.forChild({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
+],
+providers: [
+    CommonService,
+    EventService
+]
 })
 export class SearchEventsModule { }
