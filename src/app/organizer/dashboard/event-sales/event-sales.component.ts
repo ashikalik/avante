f89b522239d3a8216ab12a8f4a101b6a25277b98
malmutairi <mohammed@sbit.sa>
2019-01-29 20:27:17 +0300

    Merge remote-tracking branch 'remotes/origin/develop' into terms
    # Conflicts:
    #	src/app/organizer/dashboard/dashboard.module.ts

---
+++ src/app/organizer/dashboard/event-sales/event-sales.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import {Supervisors} from "../../../models/supervisors";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {EventoError} from "../../../models/error";
+import {OrganizerService} from "../../../api-services/organizer.service";
+import {CommonService} from "../../../api-services/common.service";
+import {ActivatedRoute} from "@angular/router";
+import {Sales} from "../../../models/sales";
+
+@Component({
+  selector: 'app-event-sales',
+  templateUrl: './event-sales.component.html',
+  styleUrls: ['./event-sales.component.scss']
+})
+export class EventSalesComponent implements OnInit {
+
+    public event_key: string;
+    public sales: Sales;
+    public showCreate: boolean;
+    public form: FormGroup;
+    public errorCreate: EventoError;
+
+
+
+    constructor(public organizerService: OrganizerService,
+                public formBuilder: FormBuilder,
+                public commonService: CommonService,
+                public activatedRoute: ActivatedRoute) {
+
+
+        this.activatedRoute.parent.params.subscribe(params => {
+            this.event_key = params['event-key']
+        })
+
+        this.showCreate = false;
+
+    }
+
+    ngOnInit() {
+        this.getEventSales();
+    }
+
+    public getEventSales() {
+        this.organizerService.getEventSales(this.event_key).subscribe(
+            res => {
+                this.sales = res;
+            }, err => {
+
+            }
+        );
+    }
+
+    public onShowCreate() {
+        if(!this.showCreate)
+            this.initAddForm()
+        this.showCreate = !this.showCreate;
+    }
+
+    public initAddForm() {
+        this.form = this.formBuilder.group(
+            {
+                'first_name': ['', Validators.compose([ Validators.required,  Validators.minLength(3), Validators.maxLength(40)])],
+                'last_name': ['', Validators.compose([Validators.required, Validators.minLength(3),Validators.maxLength(40)])],
+                'mobile': ['', Validators.compose([  Validators.required,Validators.pattern('^(05)([0-9]{8})$')])],
+                'gender': ['', Validators.compose([Validators.required])],
+                'mail': ['', Validators.compose([Validators.required, Validators.email])]
+            });
+
+    }
+
+    public addSalesStaff(form: FormGroup) {
+        this.organizerService.createEventSales(form.value, this.event_key).subscribe(
+            res => {
+                this.getEventSales();
+                this.onShowCreate();
+            }, err => {
+                this.errorCreate = err.value.error;
+            }
+        );
+    }
+}
