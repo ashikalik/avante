aa1d432b3e23d13153245cbe89155aec5b9b0961
malmutairi <mohammed@sbit.sa>
2019-01-31 22:19:06 +0300

    Merge remote-tracking branch 'remotes/origin/develop' into terms
    # Conflicts:
    #	src/app/organizer/dashboard/dashboard.module.ts

--- src/app/organizer/dashboard/root/root.component.ts
+++ src/app/organizer/dashboard/root/root.component.ts
@@ -22,38 +22,18 @@ export class RootComponent implements OnInit {
     }
 
     ngOnInit() {
-        $('.show-more').on('click', function () {
-            if ($(this).parent().prev().css('display') == 'block') {
-                $(this).parent().prev().hide();
-                $(this).html('SHOW MORE<br><i class="fas fa-chevron-down"></i>');
-            } else {
-                $(this).parent().prev().show();
-                $(this).html('SHOW LESS<br><i class="fas fa-chevron-up"></i>');
-            }
-        });
-        $('.event-image').on('click', function () {
-            $(this).parent().prev().click()
-        });
     }
 
-    onEventEdit(event) {
-        $('.event-view').hide();
-        $('.event-edit').show();
-    }
-
-    onEventSave(event) {
-        $('.event-edit').hide();
-        $('.event-view').show();
-    }
-
-    onEventCancel(event) {
-        $('.event-edit').hide();
-        $('.event-view').show();
-    }
 
     onDashboardToggle() {
         $('.side-menu').toggleClass('show');
         $('.dashboard_toggle').toggleClass('fa-bars').toggleClass('fa-times');
+    }
+
+    onMenuHide() {
+        $('.side-menu').removeClass('show');
+        $('.dashboard_toggle').toggleClass('fa-bars').toggleClass('fa-times');
+
     }
 
 }
