381337440186f2ba31bea8e2ebf21ea2449c9975
Mohammed Almoutairi <malmutairi>
2019-05-07 00:16:39 +0300

    EK-27

--- src/app/organizer/my-events/my-events.component.ts
+++ src/app/organizer/my-events/my-events.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from "../../api-services/event.service";
 import { LatestEvent } from '../../models/latest-event';
 import { Router } from '@angular/router';
-import {Meta, Title} from "@angular/platform-browser";
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-my-events',
@@ -12,26 +12,31 @@ import {Meta, Title} from "@angular/platform-browser";
 export class MyEventsComponent implements OnInit {
 
   public myEvents: LatestEvent;
+  public limit: number;
+  public page: number;
+
 
   constructor(public eventService: EventService,
     public title: Title,
     public meta: Meta,
     public router: Router) {
 
+    this.limit = 20;
+    this.page = 1;
 
   }
 
   ngOnInit() {
-      this.title.setTitle('فعالياتي');
-      this.meta.addTag({name: "description", content: 'فعالياتي'})
-      this.getMyEvents();
+    this.title.setTitle('فعالياتي');
+    this.meta.addTag({ name: "description", content: 'فعالياتي' })
+    this.getMyEvents();
 
   }
 
 
 
   public getMyEvents() {
-    this.eventService.getMyEvents().subscribe(res => {
+    this.eventService.getMyEvents(this.limit, this.page).subscribe(res => {
       this.myEvents = res;
     }, err => {
     });
@@ -44,6 +49,23 @@ export class MyEventsComponent implements OnInit {
 
   public onEvent(event_ket: string) {
     this.router.navigate(['/organizer/event/' + event_ket])
+  }
+
+
+
+  goToPage(n: number): void {
+    this.page = n;
+    this.getMyEvents();
+  }
+
+  onNext(): void {
+    this.page++;
+    this.getMyEvents();
+  }
+
+  onPrev(): void {
+    this.page--;
+    this.getMyEvents();
   }
 
 }
